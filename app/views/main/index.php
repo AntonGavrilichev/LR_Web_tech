e825b77d7598dcdc01e55f15f472fbc3397013a0
Anton <asdf.qwerty.7331@gmail.com>
2025-12-22 14:25:30 +0300

    fix lb10 UserModel

--- app/views/main/index.php
+++ app/views/main/index.php
@@ -3,7 +3,7 @@
 <div class="profile">
     <img src="<?= $photo ?? 'https://via.placeholder.com/200' ?>" alt="Фото профиля" class="profile-img">
     <div class="profile-info">
-        <h2><?= htmlspecialchars($fullName ?? 'Гавриличев Антон Александрович') ?></h2>
+        <h2><?= htmlspecialchars($fullName ?? 'Ячменев Алексей Анатольевич') ?></h2>
         <p><strong>Группа:</strong> <?= htmlspecialchars($group ?? 'ИС/22-1-з') ?></p>
         <p><strong>Лабораторная работа:</strong> <?= htmlspecialchars($labWork ?? 'Лабораторная работа №8: Исследование архитектуры MVC приложения и возможностей
 обработки данных HTML-форм на стороне сервера с использованием языка PHP') ?></p>
